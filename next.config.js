a8fed0810a0c538ac61b168118085c4fed30e5d1
Riyas Siddikk <riyass.ayath@gmail.com>
2025-08-19 14:27:52 +0530

    lock file created

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
   },
 }
 
